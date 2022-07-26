53a1a1a82b79f60b2f22dcb6fe19d3d46b8b82f4
Beatriz Gomes Barbosa <beatrizgb@ciandt.com>
2022-07-26 17:38:50 -0300

    teste

---
+++ jpa_hibernate/Pessoa.java
@@ -0,0 +1,60 @@
+package entities;
+
+import java.util.Date;
+
+public class Pessoa {
+    private Integer id;
+    private  String nome;
+    private  String email;
+    private int idade;
+    private String cpf;
+    private Date dataDeNascimento;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Date getDataDeNascimento() {
+        return dataDeNascimento;
+    }
+
+    public void setDataDeNascimento(Date dataDeNascimento) {
+        this.dataDeNascimento = dataDeNascimento;
+    }
+}
---
+++ jpa_hibernate/Produto.java
@@ -0,0 +1,49 @@
+package entities;
+
+public class Produto {
+    private Integer id;
+    private  String nome;
+    private int quantidade;
+    private double preco;
+    private boolean status;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = Boolean.parseBoolean(status);
+    }
+}
