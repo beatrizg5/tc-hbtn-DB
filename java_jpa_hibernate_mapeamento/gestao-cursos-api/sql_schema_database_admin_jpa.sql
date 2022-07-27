e6d8951f06e630867cee0a053caa3d77f67f9bdf
Beatriz Gomes Barbosa <beatrizgb@ciandt.com>
2022-07-27 16:47:43 -0300

    teste

---
+++ java_jpa_hibernate_mapeamento/gestao-cursos-api/sql_schema_database_admin_jpa.sql
@@ -0,0 +1,11 @@
+-- TABLE
+CREATE TABLE Curso (ID  integer, Nome varchar(255), Sigla varchar(255), primary key (ID));
+CREATE TABLE Endereço (ID  integer, Bairro varchar(255), CEP varchar(255), Cidade varchar(255), Endereço varchar(255), Estado varchar(255), Logradouro varchar(255), Número varchar(255), primary key (ID));
+CREATE TABLE Telefone (ID  integer, DDD varchar(255), Número varchar(255), primary key (ID));
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
