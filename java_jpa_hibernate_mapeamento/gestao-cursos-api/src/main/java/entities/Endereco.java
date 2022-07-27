a6e5a11d00e917f8e1206653715676606fbf27a5
Beatriz Gomes Barbosa <beatrizgb@ciandt.com>
2022-07-27 16:32:05 -0300

    teste

--- java_jpa_hibernate_mapeamento/gestao-cursos-api/src/main/java/entities/Endereco.java
+++ java_jpa_hibernate_mapeamento/gestao-cursos-api/src/main/java/entities/Endereco.java
@@ -11,6 +11,80 @@ public class Endereco {
     private Long id;
     @Column(name = "Logradouro")
     private String logradouro;
-    @Column(name = "Sigla")
-    private String sigla;
+    @Column(name = "Endereço")
+    private String endereco;
+    @Column(name = "Número")
+    private String numero;
+    @Column(name = "Bairro")
+    private String bairro;
+    @Column(name = "Cidade")
+    private String cidade;
+    @Column(name = "Estado")
+    private String estado;
+    @Column(name = "CEP")
+    private String cep;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
 }
